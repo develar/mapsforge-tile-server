b70413845cc7af4e35a61e758a2d0850a0a13dd0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-03-08 20:52:22 +0100

    cleanup

--- server/src/RenderedTile.kt
+++ server/src/RenderedTile.kt
@@ -3,7 +3,5 @@ package org.develar.mapsforgeTileServer
 import java.io.Serializable
 
 public class RenderedTile(public val data: ByteArray, public val lastModified: Long, public val etag: String) : Serializable {
-  public fun computeWeight(): Int {
-    return data.size + 4 + etag.length()
-  }
+  public fun computeWeight(): Int = data.size() + 4 + etag.length()
 }
--- server/src/RenderedTileSerializer.kt
+++ server/src/RenderedTileSerializer.kt
@@ -3,21 +3,17 @@ package org.develar.mapsforgeTileServer
 import org.mapdb.DataInput2
 import org.mapdb.DataOutput2
 import org.mapdb.Serializer
-
 import java.io.DataInput
 import java.io.DataOutput
-import java.io.IOException
 import java.io.Serializable
 
 class RenderedTileSerializer() : Serializer<RenderedTile>, Serializable {
-  throws(javaClass<IOException>())
   override fun serialize(out: DataOutput, value: RenderedTile) {
     DataOutput2.packLong(out, value.lastModified)
     Serializer.STRING_ASCII.serialize(out, value.etag)
     Serializer.BYTE_ARRAY.serialize(out, value.data)
   }
 
-  throws(javaClass<IOException>())
   override fun deserialize(`in`: DataInput, available: Int): RenderedTile {
     val lastModified = DataInput2.unpackLong(`in`)
     val etag = Serializer.STRING_ASCII.deserialize(`in`, available)
@@ -25,7 +21,5 @@ class RenderedTileSerializer() : Serializer<RenderedTile>, Serializable {
     return RenderedTile(data, lastModified, etag)
   }
 
-  override fun fixedSize(): Int {
-    return -1
-  }
+  override fun fixedSize() = -1
 }
